453344c00e8f4ae1efd28bcaf54b38326abf22cc
Runnan Zhang <runnanzhang@njust.edu.cn>
2025-07-12 16:26:02 +0800

    Delete CAO-SBD/functions directory

--- CAO-SBD/functions/BlindDeconvCode/normexp.m
+++
@@ -1,7 +0,0 @@
-function p=normexp(logp)
-    [n,m]=size(logp);
-    logp=logp-max(logp,[],2)*ones(1,m);
-    p=exp(logp);
-    p=p./(sum(p,2)*ones(1,m));
-    
-    
--- CAO-SBD/functions/BlindDeconvCode/resizeKer.m
+++
@@ -1,5 +0,0 @@
-function k=resizeKer(k,ret,k1,k2)
-    k=imresize(k,ret);
-    k=max(k,0);
-    k=fixsize(k,k1,k2);
-    k=k/sum(k(:));
